76e7a405e3ba6d6e9e9cb410d38ab478797c20f0
@redimpulz.s.uechi <s.uechi@redimpulz.com>
2019-04-09 16:59:46 +0900

    routed.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'hello/index'
+  get 'hello/list'
   get 'hello/view'
   root 'hello#index'
 end
